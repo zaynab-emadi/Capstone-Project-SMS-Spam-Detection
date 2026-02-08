40128fea3ae750c70b541744270d7ace1de5d4e2
zaynab_emadi <zaynabemadi@gmail.com>
2026-02-08 15:19:02 +0330

    change data paths

--- src/config.py
+++ src/config.py
@@ -2,12 +2,9 @@ import os
 
 # ==================== DATA PATHS ====================
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-DATA_RAW_PATH = "data/raw/Spam_SMS.csv"
-DATA_PROCESSED_PATH = "data/processed/"
+DATA_RAW_PATH = "../data/raw/Spam_SMS.csv"
+DATA_PROCESSED_PATH = "../data/processed/"
 
-
-# Create directories if they don't exist
-os.makedirs(DATA_PROCESSED_PATH, exist_ok=True)
 
 # ==================== DATA SPLIT RATIOS ====================
 TRAIN_SIZE = 0.6
